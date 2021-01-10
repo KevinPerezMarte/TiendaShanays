fbb6bd110f3f3049be8f863f629340f179a61c2c
KevinPerezMarte <62818389+KevinPerezMarte@users.noreply.github.com>
2021-01-10 11:13:41 -0400

    Delete enviar.php

--- enviar.php
+++
@@ -1,9 +0,0 @@
-<?php
-$destino = "tiendashanays@gmail.com";
-$nombre = $_POST["name"];
-$correo = $_POST["email"];
-$mensaje = $_POST["mensage"];
-$contenido = "Nombre: ".$nombre . "\nCorreo: ". $correo . "\nMensaje: " . $mensaje;
-mail($destino,$nombre, $contenido);
-header("Location:gracias.html")
-?>
